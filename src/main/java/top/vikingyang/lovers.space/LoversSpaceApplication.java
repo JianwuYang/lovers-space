1861a2849d4dffc6f8c9ae11c48d47293b4e39d0
JianwuYang <yangjianwu@hotmail.com>
2020-10-11 22:19:17 +0800

    后台/前端项目初始化

---
+++ src/main/java/top/vikingyang/lovers.space/LoversSpaceApplication.java
@@ -0,0 +1,13 @@
+package top.vikingyang.lovers.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoversSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoversSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/vikingyang/lovers.space/LoversSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.vikingyang.lovers.space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoversSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
