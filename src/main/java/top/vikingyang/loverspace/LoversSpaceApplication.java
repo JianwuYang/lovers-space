5867f5b3b7dc169bc288b92e68755ca7d4372674
JianwuYang <yangjianwu@hotmail.com>
2020-11-03 21:36:31 +0800

    包名修改

--- src/main/java/top/vikingyang/loverspace/LoversSpaceApplication.java
+++ src/main/java/top/vikingyang/loverspace/LoversSpaceApplication.java
@@ -1,4 +1,4 @@
-package top.vikingyang.lovers.space;
+package top.vikingyang.loverspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
