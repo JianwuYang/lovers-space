28a668b7b75b995968d46c3b24b78db6e602aa28
JianwuYang <yangjianwu@hotmail.com>
2021-01-04 21:05:25 +0800

    前端代码删除

--- src/main/webapp/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import Antd from 'ant-design-vue';
-import App from './App.vue'
-import router from "@/router";
-
-import 'ant-design-vue/dist/antd.less';
-import './assets/css/common.less'
-
-createApp(App).use(Antd).use(router).mount('#app')
